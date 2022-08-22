a3a4f432dba3456986b1641f36f8a93a42484cf7
koi <1184563958@qq.com>
2022-08-22 19:08:18 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import './assets/main.css'
+
+/* 我们传入 createApp 的对象实际上是一个组件，每个应用都需要一个“根组件”，其他组件将作为其子组件。 */
+const app = createApp(App)
+
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<script setup lang='ts'>
+import { useCounterStore } from "@/stores/counter";
+const counter = useCounterStore
+</script>
+
+
+<template>
+  <div class="about">
+    <h1>{{ counter }}</h1>
+  </div>
+</template>
+
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
